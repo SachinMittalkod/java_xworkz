13f584f61b8de57f81708b0bd6b61871dc431d69
SachinMittalkod <87868256+SachinMittalkod@users.noreply.github.com>
2021-07-28 23:03:36 +0530

    Add files via upload

---
+++ IfCondition.java
@@ -0,0 +1,14 @@
+class IfCondition{
+
+public static void main(String... args){
+     System.out.println("main started");
+  
+    int age = 5;
+    if(age>=18){
+    System.out.println("eligle for voting");
+     }
+    else
+    System.out.println("not eligible");
+  
+}
+}
